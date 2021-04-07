ffca9637c7a3095c4b0be7b8b21a973d1400da6f
Ryan <ryanpburnett@yahoo.com>
2021-04-07 14:38:40 -0500

    added API call

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
+import axios from 'axios';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <p>stuff</p>
-    </div>
-  );
+class App extends Component {
+  state = {
+    employees: []
+  }
+
+  componentDidMount() {
+    axios(`https://randomuser.me/api/?results=20`)
+      .then(res => this.setState (
+        { employees: res.data.res }))
+      .catch(err => console.error(err))
+  }
+
+  render() {
+    return(
+      <div>stuff</div>
+    )
+  }
+
 }
 
 export default App;
